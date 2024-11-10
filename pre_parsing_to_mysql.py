e1f239143aa46f7d6010fd0dca9dfb74f521c8f5
mellz12 <129605641+mellz12@users.noreply.github.com>
2024-11-10 18:35:23 +0300

    ..

--- pre_parsing_to_mysql.py
+++ pre_parsing_to_mysql.py
@@ -15,26 +15,58 @@ cursor = db.cursor()
 with open('./categories.json', 'r', encoding='utf-8') as file:
     categories_db = json.load(file)
 
-# INSERT INTO category VALUES ('
-#    id', 'category'
-# );
-category = ''
+with open("./characteristics.json", 'r', encoding='utf-8') as file:
+    characteristics_db = json.load(file)
+
 category_id = 1
 subcategory_id = 1
+characteristic_id = 1
 category_insert = ("INSERT INTO category VALUES (%s, %s)")
 subcategory_insert = ("INSERT INTO subcategory VALUES (%s, %s, %s)")
+characteristic_insert = ("INSERT INTO charcteristics VALUES (%s, %s, %s)")
 
 
 for category in categories_db.keys():
     category_data = (category_id, category)
-    cursor.execute(category_insert, category_data)
+    try:
+        cursor.execute(category_insert, category_data)
+    except:
+        print("Ошибка при добавлении категории")
     
-    db.commit()   
+    try:
+        db.commit()
+    except:
+        db.rollback
+
+   
 
     for subcategory in categories_db[category]:
         subcategory_data = (subcategory_id, subcategory, category_id)
-        cursor.execute(subcategory_insert, subcategory_data)
+        try:
+            cursor.execute(subcategory_insert, subcategory_data)
+        except:
+            print("Ошибка при добавлении подкатегории")
         subcategory_id += 1
-        db.commit()   
+
+        try:
+            db.commit()
+        except:
+            db.rollback
+
+    if subcategory in characteristics_db.keys():
+        for characteristic in characteristics_db[subcategory]:
+            characteristic_data = (characteristic_id, characteristic, subcategory_id)
+            try:
+                cursor.execute(characteristic_insert, characteristic_data)
+            except:
+                print("Ошибка при добавлении характеристики")
+            characteristic_id += 1
+
+            try:
+                db.commit()
+            except:
+                db.rollback
+
+
     category_id +=1
 db.close()
